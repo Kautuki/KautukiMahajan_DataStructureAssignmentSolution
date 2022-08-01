aff1b79cf4042e3d607a38366d34cc204d69023c
Kautuki <80846505+Kautuki@users.noreply.github.com>
2022-08-01 09:41:51 +0530

    Update Driver.java

--- Question1/Driver.java
+++ Question1/Driver.java
@@ -1,22 +1,20 @@
 package com.gldatastructure;
 
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Driver {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the total number floors in the building : ");
-        int n = sc.nextInt();
-        int x[] = new int[n+1];
-        for(int i=1;i<n+1;i++){
-            System.out.println("Enter the floor size given on day : "+i);
-            int m=sc.nextInt();
-            x[m]=i;
-        }
-        
-        SkyScrapper sl=new SkyScrapper();
-        sl.calculate(x,n);
-        
-        
-    }
+
+	public static int totalFloors;
+	public static int[] totalFloorSize;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SkyScrapper sl = new SkyScrapper();
+		sl.getInputs();
+	sl.calculateAndPrintOrderOfConstruction(sl.totalFloorSize);
+	}
 }
